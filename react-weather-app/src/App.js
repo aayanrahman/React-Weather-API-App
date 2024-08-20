b8011dbf9fa1aecf0edb8af8231d7d5a1d48fc2f
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-08-20 12:30:24 -0400

    css styling light and dark themes

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,8 +1,12 @@
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import "./styles/components/App.scss"
+
 function App() {
   return (
     <div className='App'>
-      <h1>Coding Project</h1>
-      <h2>API React weather app</h2>
+      <Header/>
+      <Main/>
     </div>
   );
 }
