b402e2f2c071f1f706ceb8da95bf3284047d0707
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-08-20 20:27:41 -0400

    Hourly Forecast Widget

--- react-weather-app/src/Components/CurrentWeather.js
+++ react-weather-app/src/Components/CurrentWeather.js
@@ -1,4 +1,5 @@
 import {getCurrentWeather} from "../api"
+import WeatherIcon from "./WeatherIcon";
 import "../styles/components/CurrentWeather.scss"
 
 function CurrentWeather() {
@@ -65,10 +66,7 @@ function CurrentWeather() {
         <div className="CurrentWeather">
             <div className="temperature">
                 <div className="weather-icon">
-                    <img 
-                        src ={`${process.env.PUBLIC_URL}/dist/weather_icons/set04/big/${icon_num}.png`}
-                        alt={summary}
-                    />
+                    <WeatherIcon iconNumber={icon_num} summary={summary}/> 
                 </div>
                 <div className="value">
                     <div className="real">{temperature} °C</div>
